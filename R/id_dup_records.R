1eac9f94465338b32724977fef73c460bb8717e5
lanescher <clanescher@gmail.com>
2025-03-25 16:00:17 -0400

    update functions

---
+++ R/id_dup_records.R
@@ -0,0 +1,69 @@
+#' Find duplicate records
+#' 
+#' @description
+#' Find PO records that might be duplicated in iNaturalist and in another dataset
+#' 
+#'
+#' @param species.data (list) output from load_species_data()
+#'
+#' @returns (vector) unique IDs of records that may be duplicates
+#' @export
+#'
+#' @examples
+
+
+
+id_dup_records <- function(species.data) {
+
+
+  POsources <- species.data$locs$cont %>%
+    st_drop_geometry() %>%
+    select(source, data.type) %>%
+    distinct() %>%
+    filter(data.type == "PO") %>%
+    pull(source)
+
+  inat.ind <- grep("iNat", POsources)
+
+  # get locations of PO data
+  locs <- bind_rows(species.data$locs$cont) %>%
+    filter(source %in% POsources) %>%
+    st_buffer(dist = 500)
+
+  # get dates of PO data
+  dates <- c()
+  for (s in 1:length(species.data$obs)) {
+    if (names(species.data$obs)[s] %in% POsources) {
+      dat <- species.data$obs[[s]] %>%
+        select(site.id, day, month, year, lat, lon, survey.id, survey.conducted, count, conus.grid.id, source, unique.id) %>%
+        mutate(site.survey.id = paste0(site.id, "-", survey.id))
+
+      dates <- bind_rows(dates, dat)
+    }
+  }
+
+  # merge locs and dates
+  locs <- full_join(locs, dates, by = c("unique.id", "survey.id", "site.id", "lat", "lon", "source", "year", "survey.conducted"))
+
+  # find all points that overlap
+
+  # split into data source and all other data sources
+  tmp1 <- filter(locs, source == POsources[inat.ind])
+  tmp2 <- filter(locs, source != POsources[inat.ind])
+
+  # find points that overlap
+  inter <- st_intersection(tmp1, tmp2)
+
+  # find points that overlap on the same date
+  inter1 <- filter(inter,
+                   #date == date.1,
+                   day == day.1,
+                   month == month.1,
+                   year == year.1) %>%
+
+    # pull site.survey.id for iNat record
+    pull(unique.id) %>%
+    unique()
+
+  return(inter1)
+}
