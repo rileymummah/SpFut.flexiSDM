858f1a95b16dad40b0ef9b87ee1feba98b85cc0d
lanescher <clanescher@gmail.com>
2026-01-23 15:40:08 -0500

    Create get_state_grid.R

---
+++ R/get_state_grid.R
@@ -0,0 +1,26 @@
+#' Get state intersections with grid
+#'
+#' @param region (list) output from make_region()
+#' @param states (data.frame) links conus.grid.id and grid.id
+#'
+#' @returns (data.frame) list of conus.grid.ids and which states they intersect with; can have multiple rows for one conus.grid.id if it overlaps multiple states
+#' @export
+#'
+#' @importFrom magrittr "%>%"
+#' @importFrom rlang .data
+#' @importFrom dplyr filter left_join mutate pull case_when
+
+get_state_grid <- function() {
+  
+  st.map <- ne_states(country = c("Canada", "Mexico", "United States of America"),
+                      returnclass = "sf") %>%
+    st_transform(st_crs(region$sp.grid))
+  suppressWarnings(stategrid <- st_intersection(region$sp.grid, st.map) %>%
+                     st_drop_geometry() %>%
+                     mutate(value = 1,
+                            name = postal,
+                            conus.grid.id = as.character(conus.grid.id)) %>%
+                     select("conus.grid.id", "name", "value"))
+  
+  return(stategrid)
+}
