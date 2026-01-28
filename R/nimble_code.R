fc691e3c45666cc9bfa8a99101fc22613301873e
lanescher <clanescher@gmail.com>
2026-01-28 11:29:55 -0500

    replace * 1 with * intercept

--- R/nimble_code.R
+++ R/nimble_code.R
@@ -307,7 +307,7 @@ for (b in 1:nCov_LETTER_NUM) {
         obs.mod1 <- gsub("_DEout", "", obs.mod1)
         obs.mod1 <- gsub("_DEin", "# _LABDE \n  _EQN", obs.mod1)
         
-        eqn <- "logit(eff_NUM[j]) <- _PARAM_NUM[1] * 1
+        eqn <- "logit(eff_NUM[j]) <- _PARAM_NUM[1] * Xw_NUM[j, 1]
   _DORE_NUM[j] <- eff_NUM[j] * S_NUM[j]"
         
         
@@ -315,7 +315,7 @@ for (b in 1:nCov_LETTER_NUM) {
         obs.mod1 <- gsub("_DEout", "# _LABDE \n_EQN", obs.mod1)
         obs.mod1 <- gsub("_DEin", "", obs.mod1)
         
-        eqn <- "logit(_DORE_NUM) <- _PARAM_NUM[1] * Xw_NUM[j]"
+        eqn <- "logit(_DORE_NUM) <- _PARAM_NUM[1] * Xw_NUM[j, 1]"
       }
       
 
