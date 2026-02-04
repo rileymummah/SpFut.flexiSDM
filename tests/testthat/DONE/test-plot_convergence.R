6688b6a98bee4fc66935018c78124090d2709df6
lanescher <clanescher@gmail.com>
2026-02-04 10:22:15 -0500

    remove summarize_samples()

--- tests/testthat/DONE/test-plot_convergence.R
+++ tests/testthat/DONE/test-plot_convergence.R
@@ -3,24 +3,7 @@ test_that("plot_convergence() works", {
   # full
   mod4 <- readRDS('~/GitHub/species-futures/pkg-tests/mod4.rds')
 
-  mod4$samples <- lapply(mod4$samples,
-                         get_derived,
-                         data = mod4$data,
-                         project = 0)
-
-  summarize_samples(samples = mod4$samples,
-                    data = mod4$data,
-                    constants = mod4$constants,
-                    project = 0,
-                    coarse.grid = F,
-                    cutoff = 0,
-                    block.out = "none",
-                    gridkey = mod4$gridkey,
-                    spatkey = NULL,
-                    effort = F,
-                    SLURM = F) -> out0
-
-  out <- plot_convergence(out0)
+  out <- plot_convergence(mod4$out)
 
   vdiffr::expect_doppelganger("convergence metrics", out$plot)
   expect_true(is.data.frame(out$dat))
