1f4a822f794187255eafbe148edeacf02056f619
lanescher <clanescher@gmail.com>
2026-02-23 11:20:24 -0500

    update with Joy's review

--- R/get_derived.R
+++ R/get_derived.R
@@ -2,7 +2,7 @@
 #'
 #' @param samples (list of mcmc objects) output from fitting a NIMBLE model
 #' @param data (NIMBLE data) data created and formatted for NIMBLE
-#' @param project (numeric) the number of projections
+#' @param project (numeric) the number of projections. NA is not accepted.
 #' @param coarse.grid (logical) whether or not a coarse grid was used to fit the spatial model
 #' @param spatRegion (list) object created from make_spatkey() and used to fit the spatial model
 #' @param pathToProj (character) path to R script where projections are set up
